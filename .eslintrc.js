3a734e527c6c55cfa8713a0b701a44af249cd956
chenfu <chenfu@tsign.cn>
2022-01-13 18:16:23 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-plusplus': 'off',
+    'no-console': 'off',
+    'no-continue': 'off',
+    'consistent-return': 'off',
+    'import/prefer-default-export': 'off',
+    'no-loop-func': 'off',
+    '@typescript-eslint/no-loop-func': 'off',
+
+    'no-param-reassign': ['error', { props: false }],
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    '@typescript-eslint/no-use-before-define': ['error', { ignoreTypeReferences: false }],
+
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: { array: false, object: true },
+        AssignmentExpression: { array: false, object: true },
+      },
+      { enforceForRenamedProperties: false },
+    ],
+
+    'lines-around-comment': [
+      'error',
+      {
+        allowArrayStart: true,
+        allowBlockStart: true,
+        allowClassStart: true,
+        allowObjectStart: true,
+        beforeBlockComment: true,
+        beforeLineComment: true,
+      },
+    ],
+
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'class', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'class' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'function' },
+      { blankLine: 'always', prev: '*', next: 'export' },
+      { blankLine: 'always', prev: 'import', next: '*' },
+      { blankLine: 'any', prev: 'import', next: 'import' },
+      { blankLine: 'any', prev: 'export', next: 'export' },
+    ],
+  },
+  ignorePatterns: ['out', 'dist', '**/*.d.ts'],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+}
